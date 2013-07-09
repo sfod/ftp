4e46e7c86347c33a2cc7ddf73aad597f028a5df8
Igor Abdrakhimov <i.abdrakhimov@telejet-soft.com>
2013-07-09 22:08:07 +0400

    Add dst_filename in protocol.

--- src/ftp_proto.c
+++ src/ftp_proto.c
@@ -8,39 +8,69 @@ int ftp_proto_parse_header(const char *s, size_t n, struct ftp_proto_t *proto,
         size_t *used)
 {
     const char *ts = s;
-    uint32_t tn;
+    uint32_t tused;
 
     switch (proto->status) {
-    /* received only part of the header */
-    case FTP_HEADER_NEED_SIZE:
+    case FTP_HEADER_NEED_SRC_LEN:
         if (proto->hlen + n < FTP_HEADER_LEN_SIZE) {
-            memcpy((char *) &proto->flen + proto->hlen, s, n);
+            memcpy((char *) &proto->src_flen + proto->hlen, s, n);
             proto->hlen += n;
-            proto->status = FTP_HEADER_NEED_SIZE;
+            proto->status = FTP_HEADER_NEED_SRC_LEN;
             return 0;
         }
-        tn = FTP_HEADER_LEN_SIZE - proto->hlen;
-        memcpy((char *) &proto->flen + proto->hlen, s, tn);
-        proto->flen = ntohl(proto->flen);
-        if (proto->flen > FTP_MAX_HEADER_SIZE - FTP_HEADER_LEN_SIZE) {
+        tused = FTP_HEADER_LEN_SIZE - proto->hlen;
+        memcpy((char *) &proto->src_flen + proto->hlen, s, tused);
+        proto->src_flen = ntohl(proto->src_flen);
+        if (proto->src_flen > FTP_MAX_HEADER_SIZE - FTP_HEADER_LEN_SIZE) {
             return -1;
         }
-        ts = s + tn;
-        n -= tn;
+        ts += tused;
+        n -= tused;
         proto->hlen = 0;
         /* fall through */
-    case FTP_HEADER_NEED_NAME:
-        if (n + proto->hlen < proto->flen) {
-            memcpy(proto->filename + proto->hlen, ts, n);
+    case FTP_HEADER_NEED_SRC_NAME:
+        if (n + proto->hlen < proto->src_flen) {
+            memcpy(proto->src_filename + proto->hlen, ts, n);
             proto->hlen += n;
-            proto->status = FTP_HEADER_NEED_NAME;
+            proto->status = FTP_HEADER_NEED_SRC_NAME;
             return 0;
         }
-        memcpy(proto->filename + proto->hlen, ts, proto->flen - proto->hlen);
-        proto->status = FTP_HEADER_COMPLETED;
-        *used = proto->flen - proto->hlen;
+        tused = proto->src_flen - proto->hlen;
+        memcpy(proto->src_filename + proto->hlen, ts, tused);
+        ts += tused;
+        n -= tused;
+        proto->hlen = 0;
+        /* fall through */
+    case FTP_HEADER_NEED_DST_LEN:
+        if (proto->hlen + n < FTP_HEADER_LEN_SIZE) {
+            memcpy((char *) &proto->dst_flen + proto->hlen, s, n);
+            proto->hlen += n;
+            proto->status = FTP_HEADER_NEED_DST_LEN;
+            return 0;
+        }
+        tused = FTP_HEADER_LEN_SIZE - proto->hlen;
+        memcpy((char *) &proto->dst_flen + proto->hlen, s, tused);
+        proto->dst_flen = ntohl(proto->dst_flen);
+        if (proto->dst_flen > FTP_MAX_HEADER_SIZE - FTP_HEADER_LEN_SIZE) {
+            return -1;
+        }
+        ts = s + tused;
+        n -= tused;
+        proto->hlen = 0;
+        /* fall through */
+    case FTP_HEADER_NEED_DST_NAME:
+        if (n + proto->hlen < proto->dst_flen) {
+            memcpy(proto->dst_filename + proto->hlen, ts, n);
+            proto->hlen += n;
+            proto->status = FTP_HEADER_NEED_DST_NAME;
+            return 0;
+        }
+        tused = proto->dst_flen - proto->hlen;
+        memcpy(proto->dst_filename + proto->hlen, ts, tused);
+        *used = tused;
         /* fall through */
     case FTP_HEADER_COMPLETED:
+        proto->status = FTP_HEADER_COMPLETED;
         return 1;
     default:
         proto->status = FTP_HEADER_INVALID;
